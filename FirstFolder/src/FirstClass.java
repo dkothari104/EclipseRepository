80850b4aae21f32b2721b48db270e6568c2c08f4
dkothari104 <dkothari@digite.com>
2016-08-25 14:49:28 +0530

    commit

--- FirstFolder/src/FirstClass.java
+++ FirstFolder/src/FirstClass.java
@@ -5,7 +5,7 @@ public class FirstClass {
 	{
 	
 		System.out.println("This is the First Java Class File created in First Folder");
-		System.out.println("Updating the Class File in ECR_1 on 25.08.2016 at 14:47");
+		System.out.println("Updating the Class File in ECR_1 on 25.08.2016 at 14:47.");
 		
 	}
 
